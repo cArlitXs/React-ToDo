f3c920bd0409cf179e69253ae20a205f4d903598
carlitosalli <carlitosalvarezlinares@gmail.com>
2019-04-11 19:21:23 +0200

    Updates

---
+++ precache-manifest.b8eff815a5b9861fc5657460d0c14301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "85ea95dd2818c2c02ed3",
+    "url": "/React-ToDo/static/css/main.8c91dc56.chunk.css"
+  },
+  {
+    "revision": "85ea95dd2818c2c02ed3",
+    "url": "/React-ToDo/static/js/main.8ab7b112.chunk.js"
+  },
+  {
+    "revision": "de23fa44dfcb11340406",
+    "url": "/React-ToDo/static/js/runtime~main.191123ef.js"
+  },
+  {
+    "revision": "7b0df846e2e28f4ebf0d",
+    "url": "/React-ToDo/static/js/2.d7c6eb4c.chunk.js"
+  },
+  {
+    "revision": "8962003ae512352483b7041e5d808f99",
+    "url": "/React-ToDo/index.html"
+  }
+];
